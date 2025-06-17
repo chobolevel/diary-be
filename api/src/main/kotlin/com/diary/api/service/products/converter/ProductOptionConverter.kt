c413faee92c6deba78288d6750d1960d42c04898
chobolevel <rodaka123@naver.com>
2025-06-17 14:29:39 +0900

    Feat: 상품 response 필드 수정

--- api/src/main/kotlin/com/diary/api/service/products/converter/ProductOptionConverter.kt
+++ api/src/main/kotlin/com/diary/api/service/products/converter/ProductOptionConverter.kt
@@ -1,6 +1,7 @@
 package com.diary.api.service.products.converter
 
 import com.diary.api.dto.products.options.CreateProductOptionRequestDto
+import com.diary.api.dto.products.options.ProductOptionResponseDto
 import com.diary.domain.entity.products.options.ProductOption
 import io.hypersistence.tsid.TSID
 import org.springframework.stereotype.Component
@@ -14,5 +15,19 @@ class ProductOptionConverter {
             name = request.name,
             order = request.order
         )
+    }
+
+    fun convert(entity: ProductOption): ProductOptionResponseDto {
+        return ProductOptionResponseDto(
+            id = entity.id,
+            name = entity.name,
+            order = entity.order,
+            createdAt = entity.createdAt!!.toInstant().toEpochMilli(),
+            updatedAt = entity.updatedAt!!.toInstant().toEpochMilli(),
+        )
+    }
+
+    fun convert(entities: List<ProductOption>): List<ProductOptionResponseDto> {
+        return entities.map { convert(it) }
     }
 }
